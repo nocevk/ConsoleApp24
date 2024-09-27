0bd2efa6ac35981d5180009c4e98792566bdbffd
ТЕХНОРАЙ <@DESKTOP-E2H7NN2>
2024-09-27 16:42:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Task1
+                Console.WriteLine("Task First");
+                Console.Write("Enter value x: ");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                double a = 3.4;
+                double b = 2.1;
+
+                double y;
+
+                if (x > 1)
+                {
+                    y = a * Math.Exp(-b * x) * Math.Cos(Math.PI * x);
+                }
+                else if (x <= -1)
+                {
+                    y = Math.Pow((x - 5), 2) * Math.Pow(Math.Pow((x + 1), 2), 1.0 / 3);
+                }
+                else
+                {
+                    y = 0;
+                }
+
+                Console.WriteLine("Result y: " + y);
+            }
+        }
+    }
+}
